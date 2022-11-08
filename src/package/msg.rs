d0bc4ffdc034fc1cd41f2bdec8d9e992350edd7f
CyberHoward <CyberHoward@protonmail.com>
2022-11-08 10:49:31 +0000

    clean up msg.rs docs

--- src/package/msg.rs
+++ src/package/msg.rs
@@ -2,34 +2,25 @@
 //!
 //! `your_namespace::template` is an add-on which allows users to ...
 //!
-//! ## Description
-//! This contract uses the proxy's value calculation configuration to get the value of the assets held in the proxy and the relative value of the deposit asset.
-//! It then mints LP tokens that are claimable for an equal portion of the proxy assets at a later date.
-//!
-//! ---
-//! **WARNING:** This mint/burn mechanism can be mis-used by flash-loan attacks if the assets contained are of low-liquidity compared to the etf's size.
-//!
 //! ## Creation
-//! The etf contract can be added on an OS by calling [`ExecuteMsg::CreateModule`](crate::manager::ExecuteMsg::CreateModule) on the manager of the os.
+//! The contract can be added on an OS by calling [`ExecuteMsg::CreateModule`](crate::manager::ExecuteMsg::CreateModule) on the manager of the os.
 //! ```ignore
-//! let etf_init_msg = InstantiateMsg{
-//!                deposit_asset: "juno".to_string(),
-//!                base: BaseInstantiateMsg{memory_address: "juno1...".to_string()},
-//!                fee: Decimal::percent(10),
-//!                provider_addr: "juno1...".to_string(),
-//!                token_code_id: 3,
-//!                etf_lp_token_name: Some("demo_etf".to_string()),
-//!                etf_lp_token_symbol: Some("DEMO".to_string()),
-//!        };
+//! let template_init_msg = InstantiateMsg::TemplateInstantiateMsg{
+//!               /// The initial value for max_count
+//!               pub max_count: Uint128,
+//!               /// Initial user counts
+//!               pub initial_counts: Option<Vec<(String, Uint128)>>,
+//!           };
+//!  
 //! let create_module_msg = ExecuteMsg::CreateModule {
 //!                 module: Module {
 //!                     info: ModuleInfo {
-//!                         name: ETF.into(),
+//!                         name: TEMPLATE.into(),
 //!                         version: None,
 //!                     },
 //!                     kind: crate::core::modules::ModuleKind::External,
 //!                 },
-//!                 init_msg: Some(to_binary(&etf_init_msg).unwrap()),
+//!                 init_msg: Some(to_binary(&template_init_msg).unwrap()),
 //!        };
 //! // Call create_module_msg on manager
 //! ```
