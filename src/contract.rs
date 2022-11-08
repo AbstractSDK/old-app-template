37e15784116c77d987131b7905f4090188b3a314
CyberHoward <CyberHoward@protonmail.com>
2022-11-08 11:50:52 +0000

    use export macro to reduce boilerplate

--- src/contract.rs
+++ src/contract.rs
@@ -1,10 +1,9 @@
 use abstract_add_on::AddOnContract;
-use abstract_sdk::abstract_os::add_on::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 use abstract_sdk::{
     ExecuteEndpoint, InstantiateEndpoint, MigrateEndpoint, QueryEndpoint, ReplyEndpoint, EXCHANGE,
 };
 use cosmwasm_std::{
-    entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Reply, Response, StdResult,
+    Binary, Deps, DepsMut, Env, MessageInfo, Reply, Response, StdResult,
 };
 
 use crate::error::TemplateError;
@@ -28,6 +27,9 @@ pub const CONTRACT_NAME: &str = "template";
 
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
+/// Expected replies
+pub const EXAMPLE_REPLY_ID: u64 = 1;
+
 /// Used as the foundation for building your addon. All entrypoints are executed through this const.
 /// `instantiate`, `query`, `execute`, `migrate`: entrypoint handlers
 /// `dependencies`:  Vec(`namespace:contract_name`) of Abstract API dependencies
@@ -36,44 +38,11 @@ const TEMPLATE_ADDON: TemplateAddOn = TemplateAddOn::new(CONTRACT_NAME, CONTRACT
     .with_query(handlers::query_handler)
     .with_execute(handlers::execute_handler)
     .with_migrate(handlers::migrate_handler)
+    .with_replies(&[(EXAMPLE_REPLY_ID,handlers::example_reply_handler)])
     .with_dependencies(&[EXCHANGE]);
+use abstract_add_on::export_endpoints;
 
-/// Instantiate entrypoint
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn instantiate(
-    deps: DepsMut,
-    env: Env,
-    info: MessageInfo,
-    msg: InstantiateMsg<TemplateInstantiateMsg>,
-) -> TemplateResult {
-    TEMPLATE_ADDON.instantiate(deps, env, info, msg)
-}
-
-/// Execute entrypoint
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn execute(
-    deps: DepsMut,
-    env: Env,
-    info: MessageInfo,
-    msg: ExecuteMsg<TemplateExecuteMsg>,
-) -> TemplateResult {
-    TEMPLATE_ADDON.execute(deps, env, info, msg)
-}
-
-/// Query entrypoint
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(deps: Deps, env: Env, msg: QueryMsg<TemplateQueryMsg>) -> StdResult<Binary> {
-    TEMPLATE_ADDON.query(deps, env, msg)
-}
-
-/// Migrate entrypoint
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn migrate(deps: DepsMut, env: Env, msg: MigrateMsg<TemplateMigrateMsg>) -> TemplateResult {
-    TEMPLATE_ADDON.migrate(deps, env, msg)
-}
-
-// Reply entrypoint
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn reply(deps: DepsMut, env: Env, msg: Reply) -> TemplateResult {
-    TEMPLATE_ADDON.reply(deps, env, msg)
-}
+// don't export endpoints when imported as library
+#[cfg(not(feature = "library"))]
+// Export the endpoints for this contract
+export_endpoints!(TEMPLATE_ADDON,TemplateAddOn);
