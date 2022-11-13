decc2e057f368156cdd9b5f2750b02917187c19d
adairrr <32375605+adairrr@users.noreply.github.com>
2022-11-13 10:12:31 -0800

    Update template script files to use a space because of liquid limitations

--- template-scripts/to-template.sh
+++ template-scripts/to-template.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # replace all occurrences of the templatized variables with their templates
-sed -i '' -e 's/"template-addon-name"/"{{addon-name}}"/g' ./**/Cargo.toml
-sed -i '' -e 's/"template_addon_name"/"{{addon_name}}"/g' ./**/Cargo.toml
-sed -i '' -e 's/"template_addon_name"/"{{addon_name}}"/g' ./**/*.rs
-sed -i '' -e 's/"template_namespace"/"{{username}}"/g' ./**/*.rs
-sed -i '' -e 's/TemplateMigrateMsg/{{addon_migrate_msg}}/g' ./**/*.rs
-sed -i '' -e 's/TemplateExecuteMsg/{{addon_execute_msg}}/g' ./**/*.rs
-sed -i '' -e 's/TemplateQueryMsg/{{addon_query_msg}}/g' ./**/*.rs
-sed -i '' -e 's/TemplateInstantiateMsg/{{addon_instantiate_msg}}/g' ./**/*.rs
-sed -i '' -e 's/TemplateResult/{{addon_result}}/g' ./**/*.rs
-sed -i '' -e 's/TemplateError/{{addon_error}}/g' ./**/*.rs
-sed -i '' -e 's/TemplateAddOn/{{addon_contract}}/g' ./**/*.rs
+sed -i '' -e 's/"template-addon-name"/" {{addon-name}}"/g' ./**/Cargo.toml
+sed -i '' -e 's/"template_addon_name"/" {{addon_name}}"/g' ./**/Cargo.toml
+sed -i '' -e 's/"template_addon_name"/" {{addon_name}}"/g' ./**/*.rs
+sed -i '' -e 's/"template_namespace"/" {{username}}"/g' ./**/*.rs
+sed -i '' -e 's/TemplateMigrateMsg/ {{addon_migrate_msg}}/g' ./**/*.rs
+sed -i '' -e 's/TemplateExecuteMsg/ {{addon_execute_msg}}/g' ./**/*.rs
+sed -i '' -e 's/TemplateQueryMsg/ {{addon_query_msg}}/g' ./**/*.rs
+sed -i '' -e 's/TemplateInstantiateMsg/ {{addon_instantiate_msg}}/g' ./**/*.rs
+sed -i '' -e 's/TemplateResult/ {{addon_result}}/g' ./**/*.rs
+sed -i '' -e 's/TemplateError/ {{addon_error}}/g' ./**/*.rs
+sed -i '' -e 's/TemplateAddOn/ {{addon_contract}}/g' ./**/*.rs
