2b903806bdea3faed789fccb14ce1d7a6e3d6eb4
adairrr <32375605+adairrr@users.noreply.github.com>
2022-11-17 13:30:19 -0800

    Cleanup

--- template-scripts/from-template.sh
+++ template-scripts/from-template.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 #
-sed -i '' -e 's/" {{app-name}}"/"template-app-name"/g' ./**/Cargo.toml
-sed -i '' -e 's/" {{app_name}}"/"template_app_name"/g' ./**/Cargo.toml
-sed -i '' -e 's/" {{app_name}}"/"template_app_name"/g' ./**/*.rs
-sed -i '' -e 's/" {{username}}"/"template_namespace"/g' ./**/*.rs
+sed -i '' -e 's/"{{app-name}}"/"template-app-name"/g' ./**/Cargo.toml
+sed -i '' -e 's/"{{app_name}}"/"template_app_name"/g' ./**/Cargo.toml
+sed -i '' -e 's/"{{app_name}}"/"template_app_name"/g' ./**/*.rs
+sed -i '' -e 's/"{{username}}"/"template_namespace"/g' ./**/*.rs
 sed -i '' -e 's/ {{app_migrate_msg}}/TemplateMigrateMsg/g' ./**/*.rs
 sed -i '' -e 's/ {{app_execute_msg}}/TemplateExecuteMsg/g' ./**/*.rs
 sed -i '' -e 's/ {{app_query_msg}}/TemplateQueryMsg/g' ./**/*.rs
